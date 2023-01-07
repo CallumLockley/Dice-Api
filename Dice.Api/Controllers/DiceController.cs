131f0f4109d4692bbb198b93c86de80d8660a1f8
Callum Lockley <lockleycallum1@gmail.com>
2023-01-07 12:21:52 +0000

    Add project files.

---
+++ Dice.Api/Controllers/DiceController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dice.Api.Controllers
+{
+    public class DiceController : Controller
+    {
+        /// <summary>
+        /// Gets random 6 sided Di value
+        /// </summary>
+        /// <returns>Face value</returns>
+        [HttpGet("GetValue")]
+        public int GetDiValue()
+        {
+            Random random = new Random();
+            return random.Next(1, 7);
+        }
+    }
+}
